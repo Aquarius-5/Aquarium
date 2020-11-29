ffedd7e04683db72c3090f5c275faefa0bfa7650
Ly Minh Trung <69301545+LyMinhTrungitdsiu19023@users.noreply.github.com>
2020-11-29 22:37:12 +0700

    Change Background and create back button

--- PlayWindow.java
+++ PlayWindow.java
@@ -3,6 +3,7 @@ package shootfeedFish;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -19,13 +20,13 @@ import javax.swing.JPanel;
 // The 3rd window or the window when customer click play button
 public class PlayWindow extends JPanel implements ActionListener,KeyListener {
 
-	JFrame window = new JFrame(); 
-	String[] a = {"Level 1 - Easy", "Level 2 - Normal", "Level 3 - Hard"};
-	JComboBox nameOfLevels = new JComboBox(a);
+	JFrame window = new JFrame("AQUARIUM"); 
+	String[] chooselevel = {"Level 1 - Easy", "Level 2 - Normal", "Level 3 - Hard"};
+	JComboBox nameOfLevels = new JComboBox(chooselevel);
 	JButton enterButton = new JButton();
 	JButton backButton = new JButton(); 
 	
-	Font customFont = new Font("Roman", Font.ROMAN_BASELINE, 30); 
+	Font customFont = new Font("Roman", Font.ROMAN_BASELINE, 20); 
 	Font comboFont = new Font("Italic", Font.ITALIC, 25);
 	
 	PlayWindow(){
@@ -34,28 +35,31 @@ public class PlayWindow extends JPanel implements ActionListener,KeyListener {
 		window.add(this);
 		
 		nameOfLevels.setFont(comboFont);
-		nameOfLevels.setForeground(Color.black);
-		nameOfLevels.setBackground(Color.white);
-		window.add(nameOfLevels, BorderLayout.NORTH);
+		nameOfLevels.setForeground(Color.BLACK);
+		window.add(nameOfLevels, BorderLayout.PAGE_START);
 		
 		
-		backButton.setFont(customFont);
-		backButton.setText("<-- BACK <--");
-		backButton.setForeground(Color.red);
-		backButton.setBackground(Color.black);
+
+		backButton.setIcon(new ImageIcon("images//back.png"));
 		backButton.addKeyListener(this);
 		backButton.addActionListener(this);;
 		window.add(backButton, BorderLayout.SOUTH);
 		
-		enterButton.setIcon(new ImageIcon("images//playlevel.jpg"));;
+		enterButton.setIcon(new ImageIcon("images//menubackground2.jpg"));
 		enterButton.addKeyListener(this);
 		enterButton.addActionListener(this);
-		window.add(enterButton,BorderLayout.CENTER);
+		window.add(enterButton, BorderLayout.CENTER); 
 		
-		window.setSize(1300,810);
-		window.setLocation(100, 10); 
+		window.setSize(1200,828);
+		window.setLocation(150, 10); 
 		window.setVisible(true);
+
 		
+	}
+	public void paint(Graphics game) {
+		ImageIcon background = new ImageIcon("images//menubackground2.jpg"); 
+		game.drawImage(background.getImage(), 0, 0, null); 
+	 
 	}
 	
 	public void keyTyped(KeyEvent e) {
@@ -101,7 +105,8 @@ public class PlayWindow extends JPanel implements ActionListener,KeyListener {
 			Level1design g1 = new Level1design();
 		}
 		else if(e.getSource() == enterButton && nameOfLevels.getSelectedIndex() == 1) {
-			JOptionPane.showMessageDialog(null, "Level 2 is not ready, it will update later");
+			window.dispose(); //closing the previous window
+			Level2design g2 = new Level2design();
 		}
 		else if(e.getSource() == enterButton && nameOfLevels.getSelectedIndex() == 2) {
 			JOptionPane.showMessageDialog(null, "Level 3 is not ready, it will update later");
